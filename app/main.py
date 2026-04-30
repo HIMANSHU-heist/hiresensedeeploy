22507aae3e84168c6872af25da478fa65256886d
Himanshu s.bendale <bendalehimanshu5@gmail.com>
2026-04-30 23:58:36 +0530

    Implement static file serving and update root endpoint
    Add static file serving for frontend assets and return index.html.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.routes import analyze
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
@@ -12,8 +15,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 app.include_router(analyze.router)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "frontend")), name="static")
 
 @app.get("/")
 def root():
-    return {"message": "HireSense AI Backend Running"}
+    return FileResponse(os.path.join(BASE_DIR, "frontend", "index.html"))
